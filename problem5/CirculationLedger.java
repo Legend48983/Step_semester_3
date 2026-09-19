a5eb2e634dc0318d370603078f456d121554127f
Aadhithya <aadhithya48983@gmail.com>
2026-09-19 16:33:59 +0530

    Add Category B solution: problem5/CirculationLedger.java

---
+++ problem5/CirculationLedger.java
@@ -0,0 +1,42 @@
+public class CirculationLedger {
+    private static final String BRANCH_CODE;
+
+    static {
+        BRANCH_CODE = "PT-MAIN";
+    }
+
+    private CirculationLedger() {
+    }
+
+    public static String processNightlyCirculation(LoanReceipt[] receipts) {
+        int processed = 0;
+        int nullSkipped = 0;
+        int referenceOnly = 0;
+        int regular = 0;
+
+        if (receipts != null) {
+            for (LoanReceipt receipt : receipts) {
+                if (receipt == null) {
+                    nullSkipped++;
+                    continue;
+                }
+
+                processed++;
+                if (receipt instanceof ReferenceOnlyLoanReceipt) {
+                    referenceOnly++;
+                } else {
+                    regular++;
+                }
+            }
+        }
+
+        return processed + " processed | "
+                + nullSkipped + " null skipped | "
+                + referenceOnly + " reference-only | "
+                + regular + " regular";
+    }
+
+    public static String getBranchCode() {
+        return BRANCH_CODE;
+    }
+}
