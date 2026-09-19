30017776943cb97d41d7f2840eee7c0e37ddcdc5
Aadhithya <aadhithya48983@gmail.com>
2026-09-19 16:42:32 +0530

    Add Week 6 inheritance and polymorphism solution

---
+++ problem6/problem3/PremiumMember.java
@@ -0,0 +1,17 @@
+public class PremiumMember extends GymMember {
+    private final String trainerName;
+
+    public PremiumMember(String memberId, int monthlyFee, String trainerName) {
+        super(memberId, monthlyFee);
+        this.trainerName = trainerName;
+    }
+
+    public String getTrainerName() {
+        return trainerName;
+    }
+
+    @Override
+    protected void chargeLateFee(int amount) {
+        super.chargeLateFee(amount / 2);
+    }
+}
